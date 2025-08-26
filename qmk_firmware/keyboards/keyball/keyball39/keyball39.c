672fcd84e608b39adb8f674145938da62496f953
satake83 <taku83@icloud.com>
2025-08-26 19:58:47 +0900

    Update keyball39.c

--- qmk_firmware/keyboards/keyball/keyball39/keyball39.c
+++ qmk_firmware/keyboards/keyball/keyball39/keyball39.c
@@ -88,12 +88,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        PASS1, PASS2, PASS3,
-        MAIL1, MAIL2, ADDRESS,
-        NOTEPAD, KC_NO, KC_NO
-    )
+#define LAYOUT_right_ball( \
+    L00, L01, L02, L03, L04,              R04, R03, PASS3, PASS2, PASS1, \
+    L10, L11, L12, L13, L14,              R14, R13, MAIL1, MAIL2, ADDRESS, \
+    L20, L21, L22, L23, L24,              NOTEPAD, R23, R22, R21, R20, \
+    L30, L31, L32, L33, L34, L35,    R35, R34,                R30  \
+    ) \
+    { \
+        {   L00,   L01,   L02,   L03,   L04, KC_NO }, \
+        {   L10,   L11,   L12,   L13,   L14, KC_NO }, \
+        {   L20,   L21,   L22,   L23,   L24, KC_NO }, \
+        {   L30,   L31,   L32,   L33,   L34,   L35 }, \
+        {   R00,   R01,   R02,   R03,   R04, KC_NO }, \
+        {   R10,   R11,   R12,   R13,   R14, KC_NO }, \
+        {   R20,   R21,   R22,   R23,   R24, KC_NO }, \
+        {   R30, KC_NO, KC_NO, KC_NO,   R34,   R35 }, \
+    }
 };
-
-
-
-
