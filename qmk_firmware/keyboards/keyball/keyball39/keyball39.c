2dc03013e0f84616dc4943a61a1451bf2edb477d
satake83 <taku83@icloud.com>
2025-08-26 20:02:40 +0900

    Update keyball39.c

--- qmk_firmware/keyboards/keyball/keyball39/keyball39.c
+++ qmk_firmware/keyboards/keyball/keyball39/keyball39.c
@@ -87,12 +87,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-#define LAYOUT_right_ball( \
+    [0] = LAYOUT_right_ball( \
     L00, L01, L02, L03, L04,              R04, R03, PASS3, PASS2, PASS1, \
     L10, L11, L12, L13, L14,              R14, R13, MAIL1, MAIL2, ADDRESS, \
     L20, L21, L22, L23, L24,              NOTEPAD, R23, R22, R21, R20, \
-    L30, L31, L32, L33, L34, L35,    R35, R34,                R30  \
+    L30, L31, L32, L33, L34, L35,    R35, R34,                    R30  \
     ) \
     { \
         {   L00,   L01,   L02,   L03,   L04, KC_NO }, \
