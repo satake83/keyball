b77ee32e71fd13cc0a11c4254317231c9b209665
satake83 <taku83@icloud.com>
2025-08-28 17:45:52 +0900

    Update keyball39.c

--- qmk_firmware/keyboards/keyball/keyball39/keyball39.c
+++ qmk_firmware/keyboards/keyball/keyball39/keyball39.c
@@ -19,9 +19,3 @@ matrix_row_t matrix_mask[MATRIX_ROWS] = {
 
 void keyball_on_adjust_layout(keyball_adjust_t v) {
 }
-
-const char code_to_name[] PROGMEM = {
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-    'U', 'V', 'W', 'X', 'Y', 'Z'
-};
