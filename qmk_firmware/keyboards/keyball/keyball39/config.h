23dc842a1ca7c9e89199f0fa435762502598aecc
satake83 <taku83@icloud.com>
2025-08-28 18:26:18 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/config.h
@@ -31,6 +31,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SOFT_SERIAL_PIN         D2
 #define SPLIT_HAND_MATRIX_GRID  F6, B5
 #define SPLIT_USB_DETECT
+#ifdef OLED_ENABLE
+#    define SPLIT_OLED_ENABLE
+#endif
 
 // If your PC does not recognize Keyball, try setting this macro. This macro
 // increases the firmware size by 200 bytes, so it is disabled by default, but
@@ -39,7 +42,33 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define SPLIT_TRANSACTION_IDS_KB KEYBALL_GET_INFO, KEYBALL_GET_MOTION, KEYBALL_SET_CPI
 
+// RGB LED settings
+#define WS2812_DI_PIN       D3
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLED_NUM      48
+#    define RGBLED_SPLIT    { 24, 24 }  // (24 + 22)
+#    ifndef RGBLIGHT_LIMIT_VAL
+#        define RGBLIGHT_LIMIT_VAL  150 // limitated for power consumption
+#    endif
+#    ifndef RGBLIGHT_VAL_STEP
+#        define RGBLIGHT_VAL_STEP   15
+#    endif
+#    ifndef RGBLIGHT_HUE_STEP
+#        define RGBLIGHT_HUE_STEP   17
+#    endif
+#    ifndef RGBLIGHT_SAT_STEP
+#        define RGBLIGHT_SAT_STEP   17
+#    endif
+#endif
+#ifdef RGB_MATRIX_ENABLE
+#    define RGB_MATRIX_SPLIT    { 24, 24 }
+#endif
 
+#ifndef OLED_FONT_H
+#    define OLED_FONT_H "keyboards/keyball/lib/logofont/logofont.c"
+#    define OLED_FONT_START 32
+#    define OLED_FONT_END 195
+#endif
 
 #if !defined(LAYER_STATE_8BIT) && !defined(LAYER_STATE_16BIT) && !defined(LAYER_STATE_32BIT)
 #    define LAYER_STATE_8BIT
