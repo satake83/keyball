87f65743ad19bf5945f3ab3270da89bc2006c848
satake83 <taku83@icloud.com>
2025-08-28 17:49:29 +0900

    Update keyball.c

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -779,9 +779,3 @@ uint8_t mod_config(uint8_t mod) {
 }
 
 #endif
-
-const char code_to_name[] PROGMEM = {
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-    'U', 'V', 'W', 'X', 'Y', 'Z'
-};
