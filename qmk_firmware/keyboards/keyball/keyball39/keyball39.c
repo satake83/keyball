a85fd81cb1dd6ab7a14863d8855ae0eefa7c81b3
satake83 <taku83@icloud.com>
2025-08-28 17:38:32 +0900

    Update keyball39.c

--- qmk_firmware/keyboards/keyball/keyball39/keyball39.c
+++ qmk_firmware/keyboards/keyball/keyball39/keyball39.c
@@ -1,21 +1,3 @@
-/*
-Copyright 2021 @Yowkees
-Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include QMK_KEYBOARD_H
 
 #include "lib/keyball/keyball.h"
@@ -37,3 +19,9 @@ matrix_row_t matrix_mask[MATRIX_ROWS] = {
 
 void keyball_on_adjust_layout(keyball_adjust_t v) {
 }
+
+const char code_to_name[] PROGMEM = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z'
+};
