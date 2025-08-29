58c01cc5fe0644a2a9caea2842d2723c7298433b
satake83 <taku83@icloud.com>
2025-08-29 11:42:02 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -141,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS  , KC_TRNS   , KC_TRNS  , KC_TRNS    , KC_TRNS    ,                                      KC_TRNS    , KC_TRNS   ,  KC_TRNS  , KC_TRNS  , KC_TRNS   ,
     KC_TRNS  , KC_TRNS   , KC_TRNS  , KC_TRNS    , KC_TRNS    ,                                      KC_TRNS    , KC_TRNS   ,  KC_TRNS  , KC_TRNS  , KC_TRNS   ,
     KC_TRNS  , KC_TRNS   , KC_TRNS  , KC_TRNS    , KC_TRNS    ,                                      KC_TRNS    , KC_TRNS   ,  KC_TRNS  , KC_TRNS  , KC_TRNS   ,
-    KC_TRNS  , KC_TRNS   , KC_TRNS  , KC_TRNS    , KC_TRNS    , KC_TRNS  ,            　  SKC_DEL  , KC_TRNS    , KC_TRNS   ,  KC_TRNS  , KC_TRNS  , KC_TRNS  
+    KC_TRNS  , KC_TRNS   , KC_TRNS  , KC_TRNS    , KC_TRNS    , KC_TRNS  ,            　  KC_DEL   , KC_TRNS    , KC_TRNS   ,  KC_TRNS  , KC_TRNS  , KC_TRNS  
   ),
 };
 
